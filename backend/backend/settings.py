d2bdae18548f809abefdcc009af7a6e51d8ed95e
imaksx <imaksxkanal@gmail.com>
2024-10-18 21:18:08 +0300

    Added setup file

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -1,5 +1,5 @@
-from pathlib import Path
 import os
+from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -9,11 +9,11 @@ SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^
 DEBUG = True
 
 ALLOWED_HOSTS = [
-	'localhost',
-	'127.0.0.1',
-	'84.201.163.241',
-	'imaksxhost.bounceme.net',
-		 
+    'localhost',
+    '127.0.0.1',
+    '84.201.163.241',
+    'imaksxhost.bounceme.net',
+
 ]
 
 
